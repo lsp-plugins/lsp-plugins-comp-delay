2a9aa994e64e4fcb9944451726a8716bec5c5a85
sadko4u <sadko4u@gmail.com>
2020-11-25 22:05:31 +0300

    Initial commit

---
+++ include/private/meta/comp_delay_t.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-comp-delay
+ * Created on: 25 нояб. 2020 г.
+ *
+ * lsp-plugins-comp-delay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-comp-delay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-comp-delay. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef PRIVATE_META_COMP_DELAY_H_
+#define PRIVATE_META_COMP_DELAY_H_
+
+#include <lsp-plug.in/plug-fw/meta/types.h>
+
+namespace lsp
+{
+    //-------------------------------------------------------------------------
+    // Compensation delay metadata
+    namespace meta
+    {
+        typedef struct comp_delay_t
+        {
+            static const float  METERS_MIN          = 0.0f;
+            static const float  METERS_MAX          = 200.0f;
+            static const float  METERS_DFL          = 0.0f;
+            static const float  METERS_STEP         = 1.0f;
+
+            static const float  CENTIMETERS_MIN     = 0.0f;
+            static const float  CENTIMETERS_MAX     = 100.0f;
+            static const float  CENTIMETERS_DFL     = 0.0f;
+            static const float  CENTIMETERS_STEP    = 0.1f;
+
+            static const float  SAMPLES_MIN         = 0.0f;
+            static const float  SAMPLES_MAX         = 10000.0f;
+            static const float  SAMPLES_DFL         = 0.0f;
+            static const float  SAMPLES_STEP        = 1.0f;
+
+            static const float  TIME_MIN            = 0.0f;
+            static const float  TIME_MAX            = 1000.0f;
+            static const float  TIME_DFL            = 0.0f;
+            static const float  TIME_STEP           = 0.01f;
+
+            static const float  TEMPERATURE_MIN     = -60.0f;
+            static const float  TEMPERATURE_MAX     = +60.0f;
+            static const float  TEMPERATURE_DFL     = 20.0f;
+            static const float  TEMPERATURE_STEP    = 0.1f;
+
+            static const float  DELAY_OUT_MAX_TIME      = 1000 /* TIME_MAX [ms] */;
+            static const float  DELAY_OUT_MAX_SAMPLES   = 1000 /* TIME_MAX [ms] */ * 0.001 /* [ s/ms ] */ * MAX_SAMPLE_RATE /* [samples / s] */;
+            static const float  DELAY_OUT_MAX_DISTANCE  = 1000 /* TIME_MAX [ms] */ * 0.001 /* [ s/ms ] */ * MAX_SOUND_SPEED /* [m/s] */ * 100 /* [cm / m ] */;
+
+            enum modes
+            {
+                M_SAMPLES,
+                M_DISTANCE,
+                M_TIME
+            };
+        } comp_delay_t;
+
+        // Plugin metadata
+        extern const plugin_t comp_delay_mono;
+        extern const plugin_t comp_delay_stereo;
+        extern const plugin_t comp_delay_x2_stereo;
+    }
+}
+
+#endif /* PRIVATE_META_COMP_DELAY_H_ */
---
+++ src/main/meta/comp_delay_t.cpp
@@ -0,0 +1,208 @@
+/*
+ * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
+ *
+ * This file is part of lsp-plugins-comp-delay
+ * Created on: 25 нояб. 2020 г.
+ *
+ * lsp-plugins-comp-delay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * lsp-plugins-comp-delay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with lsp-plugins-comp-delay. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <lsp-plug.in/plug-fw/meta/ports.h>
+#include <private/meta/comp_delay_t.h>
+
+namespace lsp
+{
+    namespace meta
+    {
+        //-------------------------------------------------------------------------
+        // Compensation delay
+        static const port_item_t comp_delay_modes[] =
+        {
+            { "Samples",    "comp_delay.samples"    },
+            { "Distance",   "comp_delay.distance"   },
+            { "Time",       "comp_delay.time"       },
+            { NULL, NULL }
+        };
+
+        static const port_t comp_delay_mono_ports[] =
+        {
+            // Input audio ports
+            AUDIO_INPUT_MONO,
+
+            // Output audio ports
+            AUDIO_OUTPUT_MONO,
+
+            // Input controls
+            BYPASS,
+
+            COMBO("mode", "Mode", 0, comp_delay_modes),
+            SWITCH("ramp", "Ramping", 0.0f),
+            INT_CONTROL("samp", "Samples", U_SAMPLES, comp_delay_t::SAMPLES),
+            INT_CONTROL("m", "Meters", U_M, comp_delay_t::METERS),
+            CONTROL("cm", "Centimeters", U_CM, comp_delay_t::CENTIMETERS),
+            CONTROL("t", "Temperature", U_DEG_CEL, comp_delay_t::TEMPERATURE),
+            CONTROL("time", "Time", U_MSEC, comp_delay_t::TIME),
+            DRY_GAIN(0.0f),
+            WET_GAIN(1.0f),
+
+            OUT_GAIN,
+
+            // Output controls
+            METER_MINMAX("d_t", "Delay time", U_HZ, 0.0f, comp_delay_t::DELAY_OUT_MAX_TIME),
+            METER_MINMAX("d_s", "Delay samples", U_SAMPLES, 0.0f, comp_delay_t::DELAY_OUT_MAX_SAMPLES),
+            METER_MINMAX("d_d", "Delay distance", U_CM, 0.0f, comp_delay_t::DELAY_OUT_MAX_DISTANCE),
+
+            PORTS_END
+        };
+
+        static const port_t comp_delay_stereo_ports[] =
+        {
+            // Input audio ports
+            AUDIO_INPUT_LEFT,
+            AUDIO_INPUT_RIGHT,
+
+            // Output audio ports
+            AUDIO_OUTPUT_LEFT,
+            AUDIO_OUTPUT_RIGHT,
+
+            // Input controls
+            BYPASS,
+
+            COMBO("mode", "Mode", 0, comp_delay_modes),
+            SWITCH("ramp", "Ramping", 0.0f),
+            INT_CONTROL("samp", "Samples", U_SAMPLES, comp_delay_t::SAMPLES),
+            INT_CONTROL("m", "Meters", U_M, comp_delay_t::METERS),
+            CONTROL("cm", "Centimeters", U_CM, comp_delay_t::CENTIMETERS),
+            CONTROL("t", "Temperature", U_DEG_CEL, comp_delay_t::TEMPERATURE),
+            CONTROL("time", "Time", U_MSEC, comp_delay_t::TIME),
+            DRY_GAIN(0.0f),
+            WET_GAIN(1.0f),
+
+            OUT_GAIN,
+
+            // Output controls
+            METER_MINMAX("d_t", "Delay time", U_HZ, 0.0f, comp_delay_t::DELAY_OUT_MAX_TIME),
+            METER_MINMAX("d_s", "Delay samples", U_SAMPLES, 0.0f, comp_delay_t::DELAY_OUT_MAX_SAMPLES),
+            METER_MINMAX("d_d", "Delay distance", U_CM, 0.0f, comp_delay_t::DELAY_OUT_MAX_DISTANCE),
+
+            PORTS_END
+        };
+
+        static const port_t comp_delay_x2_stereo_ports[] =
+        {
+            // Input audio ports
+            AUDIO_INPUT_LEFT,
+            AUDIO_INPUT_RIGHT,
+
+            // Output audio ports
+            AUDIO_OUTPUT_LEFT,
+            AUDIO_OUTPUT_RIGHT,
+
+            // Input controls
+            BYPASS,
+
+            COMBO("mode_l", "Mode L", 0, comp_delay_modes),
+            SWITCH("ramp_l", "Ramping L", 0.0f),
+            INT_CONTROL("samp_l", "Samples L", U_SAMPLES, comp_delay_t::SAMPLES),
+            INT_CONTROL("m_l", "Meters L", U_M, comp_delay_t::METERS),
+            CONTROL("cm_l", "Centimeters L", U_CM, comp_delay_t::CENTIMETERS),
+            CONTROL("t_l", "Temperature L", U_DEG_CEL, comp_delay_t::TEMPERATURE),
+            CONTROL("time_l", "Time L", U_MSEC, comp_delay_t::TIME),
+            DRY_GAIN_L(0.0f),
+            WET_GAIN_L(1.0f),
+
+            COMBO("mode_r", "Mode R", 0, comp_delay_modes),
+            SWITCH("ramp_r", "Ramping R", 0.0f),
+            INT_CONTROL("samp_r", "Samples R", U_SAMPLES, comp_delay_t::SAMPLES),
+            INT_CONTROL("m_r", "Meters R", U_M, comp_delay_t::METERS),
+            CONTROL("cm_r", "Centimeters R", U_CM, comp_delay_t::CENTIMETERS),
+            CONTROL("t_r", "Temperature R", U_DEG_CEL, comp_delay_t::TEMPERATURE),
+            CONTROL("time_r", "Time R", U_MSEC, comp_delay_t::TIME),
+            DRY_GAIN_R(0.0f),
+            WET_GAIN_R(1.0f),
+
+            OUT_GAIN,
+
+            // Output controls
+            METER_MINMAX("d_t_l", "Delay time L", U_HZ, 0.0f, comp_delay_t::DELAY_OUT_MAX_TIME),
+            METER_MINMAX("d_s_l", "Delay samples L", U_SAMPLES, 0.0f, comp_delay_t::DELAY_OUT_MAX_SAMPLES),
+            METER_MINMAX("d_d_l", "Delay distance L", U_CM, 0.0f, comp_delay_t::DELAY_OUT_MAX_DISTANCE),
+
+            METER_MINMAX("d_t_r", "Delay time R", U_HZ, 0.0f, comp_delay_t::DELAY_OUT_MAX_TIME),
+            METER_MINMAX("d_s_r", "Delay samples R", U_SAMPLES, 0.0f, comp_delay_t::DELAY_OUT_MAX_SAMPLES),
+            METER_MINMAX("d_d_r", "Delay distance R", U_CM, 0.0f, comp_delay_t::DELAY_OUT_MAX_DISTANCE),
+
+            PORTS_END
+        };
+
+        static const int comp_delay_classes[] = { C_DELAY, -1 };
+
+        const plugin_t comp_delay_mono =
+        {
+            "Verzögerungsausgleicher Mono",
+            "Delay Compensator Mono",
+            "VA1M",
+            &developers::v_sadovnikov,
+            "comp_delay_mono",
+            "jav8",
+            LSP_COMP_DELAY_BASE + 0,
+            LSP_MODULE_VERSION(1, 1, 0),
+            comp_delay_classes,
+            E_NONE,
+            comp_delay_mono_ports,
+            "delay/comp/mono.xml",
+            NULL,
+            mono_plugin_port_groups
+        };
+
+        const plugin_t comp_delay_stereo =
+        {
+            "Verzögerungsausgleicher Stereo",
+            "Delay Compensator Stereo",
+            "VA1S",
+            &developers::v_sadovnikov,
+            "comp_delay_stereo",
+            "qpwr",
+            LSP_COMP_DELAY_BASE + 1,
+            LSP_MODULE_VERSION(1, 1, 0),
+            comp_delay_classes,
+            E_NONE,
+            comp_delay_stereo_ports,
+            "delay/comp/stereo.xml",
+            NULL,
+            stereo_plugin_port_groups
+        };
+
+        const plugin_t comp_delay_x2_stereo =
+        {
+            "Verzögerungsausgleicher x2 Stereo",
+            "Delay Compensator x2 Stereo",
+            "VA2S",
+            &developers::v_sadovnikov,
+            "comp_delay_x2_stereo",
+            "fwd3",
+            LSP_COMP_DELAY_BASE + 2,
+            LSP_MODULE_VERSION(1, 1, 0),
+            comp_delay_classes,
+            E_NONE,
+            comp_delay_x2_stereo_ports,
+            "delay/comp/x2_stereo.xml",
+            NULL,
+            stereo_plugin_port_groups
+        };
+    }
+}
+
+
