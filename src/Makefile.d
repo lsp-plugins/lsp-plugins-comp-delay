0e120768c02871c4f308874aa678080aea04b36c
sadko4u <sadko4u@gmail.com>
2022-05-03 17:48:25 +0300

    Updated build dependencies

--- src/Makefile.d
+++ src/Makefile.d
@@ -43,6 +43,7 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/plug/comp_delay.o: \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/types.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/version.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/spec.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/ICanvasFactory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/IWrapper.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/core/KVTStorage.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/parray.h \
@@ -55,12 +56,14 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/plug/comp_delay.o: \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/io/IInSequence.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/io/IInStream.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/io/Path.h \
+ $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/resource/PrefixLoader.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/Module.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/Factory.h \
  $(LSP_PLUGINS_COMP_DELAY_INC)/private/meta/comp_delay.h \
  $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/units.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/stdlib/math.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/common/alloc.h \
+ $(LSP_COMMON_LIB_INC)/lsp-plug.in/stdlib/stdlib.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/dsp.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/version.h \
  $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/types.h \
@@ -138,6 +141,7 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/ui/comp_delay.o: \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/types.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/version.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/spec.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/ICanvasFactory.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/IWrapper.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/core/KVTStorage.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/parray.h \
@@ -150,6 +154,7 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/ui/comp_delay.o: \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/io/IInSequence.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/io/IInStream.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/io/Path.h \
+ $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/resource/PrefixLoader.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/Module.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/plug/Factory.h \
  $(LSP_PLUGINS_COMP_DELAY_INC)/private/meta/comp_delay.h \
@@ -158,7 +163,6 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/ui/comp_delay.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IPortListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IWrapper.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/resource/PrefixLoader.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/resource/Environment.h \
  $(LSP_LLTL_LIB_INC)/lsp-plug.in/lltl/pphash.h \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/tk.h \
@@ -277,8 +281,8 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/ui/comp_delay.o: \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/util/URLSink.h \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/base/Widget.h \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/base/Registry.h \
- $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/base/WidgetContainer.h \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/cast.h \
+ $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/base/WidgetContainer.h \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/simple/Button.h \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/simple/CheckBox.h \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/simple/Edit.h \
@@ -330,31 +334,81 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/ui/comp_delay.o: \
  $(LSP_TK_LIB_INC)/lsp-plug.in/tk/widgets/dialogs/FileDialog.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/fmt/bookmarks.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/io/IOutSequence.h \
+ $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/expr/Variables.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Module.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/SwitchedPort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ValuePort.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/IKVTListener.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/fmt/config/PullParser.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/fmt/config/types.h \
+ $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/fmt/config/Serializer.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ISchemaListener.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PortResolver.h \
  $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/expr/Expression.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ControlPort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/PathPort.h \
- $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/ValuePort.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/Factory.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIOverrides.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ui/UIContext.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/types.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/parse.h \
  $(LSP_COMMON_LIB_INC)/lsp-plug.in/stdlib/stdlib.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/helpers.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Property.h \
- $(LSP_RUNTIME_LIB_INC)/lsp-plug.in/expr/Variables.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Integer.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Float.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Boolean.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/LCString.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Expression.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Color.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/dsp.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/version.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/types.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/3dmath.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/3dmath/types.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/coding.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/complex.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/context.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/convolution.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/copy.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/fastconv.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/fft.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/filters.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/filters/types.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/filters/dynamic.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/filters/static.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/filters/transfer.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/filters/transform.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/float.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/graphics.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/hmath.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/hmath/hdotp.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/hmath/hsum.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/misc.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/mix.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/msmatrix.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pcomplex.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/abs_vv.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/exp.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/fmop_kx.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/fmop_vv.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/log.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/minmax.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/op_kx.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/op_vv.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/pmath/pow.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/resampling.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/search.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/search/iminmax.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/search/minmax.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/smath.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/interpolation.h \
+ $(LSP_DSP_LIB_INC)/lsp-plug.in/dsp/common/interpolation/linear.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Embedding.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Padding.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Enum.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/util/Direction.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/Widget.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/Registry.h \
@@ -363,6 +417,7 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/ui/comp_delay.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/PluginWindow.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Void.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Bevel.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Edit.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Label.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Knob.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Button.h \
@@ -372,6 +427,7 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/ui/comp_delay.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Separator.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Hyperlink.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/Fader.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/simple/ProgressBar.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/containers/Box.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/containers/Align.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/containers/Group.h \
@@ -385,7 +441,32 @@ $(LSP_PLUGINS_COMP_DELAY_BIN)/main/ui/comp_delay.o: \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/graph/Axis.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/graph/Marker.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/graph/Text.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/graph/Dot.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/graph/Mesh.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/graph/FBuffer.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/3d/types.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/3d/Area3D.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/view/types.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/3d/Object3D.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/bsp/context.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/Object3D.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/types.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/Scene3D.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/Allocator3D.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/bsp/types.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/3d/Origin3D.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/3d/Mesh3D.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/3d/Source3D.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/rt/types.h \
+ $(LSP_DSP_UNITS_INC)/lsp-plug.in/dsp-units/3d/raytrace.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/3d/Capture3D.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/3d/Model3D.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/AudioSample.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/FileButton.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/Fraction.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/LedChannel.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/LedMeter.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/MidiNote.h \
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/TempoTap.h \
  $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/Rack.h \
- $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/AudioSample.h
+ $(LSP_PLUGIN_FW_INC)/lsp-plug.in/plug-fw/ctl/specific/ThreadComboBox.h
